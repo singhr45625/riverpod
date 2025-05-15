7b36e6d5da8dc042923516b18dced0945af019ce
singhr45625 <kpuja0969@gmail.com>
2025-05-15 16:25:05 +0530

    cc

---
+++ lib/providers/cart_providers.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_files/models/product.dart';
+
+class CartNotifier extends Notifier<Set<Product>> {
+  // initial value
+  @override
+  Set<Product> build() {
+     return const{
+       Product(
+           id: '8',
+           title: 'Electric Guitar',
+           price: 45,
+           image: 'assets/products/guitar.png'
+       ),
+     };
+  }
+  //updating method
+
+ void addProduct(Product product) {
+    if (!state.contains(product)) {
+      state = {...state, product};
+    }
+ }
+
+ void removeProduct(Product product) {
+    if(state.contains(product)) {
+      state = state.where((p) => p.id != product.id).toSet();
+    }
+ }
+}
+
+final cartNotifierProvider = NotifierProvider<CartNotifier, Set<Product>>(() {
+  return CartNotifier();
+});
